6d62392e001e4b3e344aacfc56f0b7e7b5d16b01
Chris Booth <31346759+chrisPhat@users.noreply.github.com>
2021-11-12 16:45:52 +0000

    Added P5 code of unsmiley face

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,37 @@
+var canvasSize = 400;
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(canvasSize, canvasSize);
     canvas.parent("p5container");
 }
 
 function draw() {
+    var eyeX = map(mouseX, 0, 400, 125, 175);
+    var eyeY = map(mouseY, 0, 400, 125, 175);
+    //console.log(mouseX, mouseY);
+
     background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    stroke(100); //Stroke Color or Fill
+    strokeWeight(5); //Stroke Thickness
+
+    fill(255, 255, 0);
+    ellipse(width / 2, height / 2, 300);
+
+    //eyes
+    fill(255);
+    ellipse(eyeX, eyeY, 45, 60);
+    ellipse(eyeX + 100, eyeY, 45, 60);
+
+    fill(255, 215, 0);
+    noStroke();
+    ellipse(110, 220, 45);
+    ellipse(290, 220, 45);
+
+    stroke(100); //Stroke Color or Fill
+    strokeWeight(5); //Stroke Thickness
+
+    noFill();
+    arc(200, 350, 180, 180, radians(210), radians(330));
+
 }
